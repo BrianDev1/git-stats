eb1b799e3f8cdbc83c44db3cd4509161a5249fd9
Brian Rudolph <brian.rudolph303@gmail.com>
2021-09-18 20:28:39 -0600

    fix: Convert to class server

---
+++ src/server.ts
@@ -0,0 +1,45 @@
+import * as http from "http";
+import express, { Express } from "express";
+import bodyParser from "body-parser";
+
+export class Server {
+  private _app: Express;
+  private _server: http.Server;
+
+  get server(): http.Server {
+    return this._server;
+  }
+  constructor() {
+    this._app = express();
+    this._app = this._app.set("port", process.env.PORT || 3000);
+
+    this.configMiddleware();
+  }
+
+  public configMiddleware() {
+    this._app.use(bodyParser.json());
+    this._app.use(bodyParser.urlencoded({ extended: true }));
+
+    // CORS
+    this._app.use(function (req, res, next) {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,OPTIONS,POST,PUT"
+      );
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization"
+      );
+      next();
+    });
+  }
+
+  public start() {
+    const port = this._app.get("port");
+    this._server = this._app.listen(port, () => {
+      console.log(`🚀 Server is running on port ${port}`);
+    });
+  }
+}
